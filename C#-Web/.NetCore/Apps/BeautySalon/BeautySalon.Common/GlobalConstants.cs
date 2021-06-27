a7a26f1f1f451daffc53616c0347352832e12085
Dimitar Vezalov <wopopo13@gmail.com>
2021-06-27 12:58:17 +0300

    Initial Commit

---
+++ C#-Web/.NetCore/Apps/BeautySalon/BeautySalon.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BeautySalon.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "BeautySalon";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
